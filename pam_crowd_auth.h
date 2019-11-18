13ab104cb8a7a338e10a41c3780d248888f108aa
Mark Bidewell <mbidewel@gmail.com>
2019-11-17 21:16:48 -0500

    Refactoring and configuration

--- pam_crowd_auth.h
+++ pam_crowd_auth.h
@@ -1,7 +1,7 @@
 #ifndef PAM_CROWD_AUTH
 #define PAM_CROWD_AUTH
 
-#define CROWD_AUTH_URL "http://<crowd_base_url>/crowd/rest/usermanagement/1/authentication?username=%s"
+#define CROWD_AUTH_URL "%s/crowd/rest/usermanagement/1/authentication?username=%s"
 #define CROWD_AUTH_BODY "{\"value\": \"%s\"}"
 
 #endif
